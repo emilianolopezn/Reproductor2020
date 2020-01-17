0fe711f2e162191f41617e6ed8e7dee93d8bbf1e
Armando Meza Leyva <armezaleyva@gmail.com>
2020-01-17 09:45:24 -0700

    Pausa y Detener

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 
 using Microsoft.Win32;
 
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace Reproductor
 {
     /// <summary>
@@ -22,9 +25,32 @@ namespace Reproductor
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Lector de archivos
+        AudioFileReader reader;
+        //Comunicacion con la tarjeta de audio
+        //exclusivo para salidas
+        WaveOut output;
+
         public MainWindow()
         {
             InitializeComponent();
+            ListarDispositivosSalida();
+            btnReproducir.IsEnabled = false;
+            btnDetener.IsEnabled = false;
+            btnPausa.IsEnabled = false;
+        }
+
+        void ListarDispositivosSalida()
+        {
+            cbDispositivoSalida.Items.Clear();
+            for(int i=0; i < WaveOut.DeviceCount; i++)
+            {
+                WaveOutCapabilities capacidades =
+                    WaveOut.GetCapabilities(i);
+                cbDispositivoSalida.Items.Add(
+                    capacidades.ProductName);
+            }
+            cbDispositivoSalida.SelectedIndex = 0;
         }
 
         private void btnExaminar_Click(object sender, RoutedEventArgs e)
@@ -35,8 +61,75 @@ namespace Reproductor
             {
                 txtRutaArchivo.Text =
                     openFileDialog.FileName;
+                btnReproducir.IsEnabled = true;
             }
 
+        }
+
+        private void btnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (output != null && 
+                output.PlaybackState == PlaybackState.Paused)
+            {
+                // retomo reproduccion
+                output.Play();
+                btnReproducir.IsEnabled = false;
+                btnPausa.IsEnabled = true;
+                btnDetener.IsEnabled = true;
+            } else
+            {
+                if (txtRutaArchivo.Text != null &&
+                txtRutaArchivo.Text != string.Empty)
+                {
+                    reader =
+                        new AudioFileReader(txtRutaArchivo.Text);
+                    output = new WaveOut();
+                    output.DeviceNumber =
+                        cbDispositivoSalida.SelectedIndex;
+                    output.PlaybackStopped += Output_PlaybackStopped;
+                    output.Init(reader);
+                    output.Play();
+
+                    btnReproducir.IsEnabled = false;
+                    btnPausa.IsEnabled = true;
+                    btnDetener.IsEnabled = true;
+
+                    lblTiempoTotal.Text =
+                        reader.TotalTime.ToString().
+                        Substring(0, 8);
+                }
+            }
+
+            
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            reader.Dispose();
+            output.Dispose();
+        }
+
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Stop();
+                btnReproducir.IsEnabled = true;
+                btnPausa.IsEnabled = false;
+                btnDetener.IsEnabled = false;
+            }
+        }
+
+        private void btnPausa_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Pause();
+                btnReproducir.IsEnabled = true;
+                btnPausa.IsEnabled = false;
+                btnDetener.IsEnabled = true;
+            }
         }
     }
 }
